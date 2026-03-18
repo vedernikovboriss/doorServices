0d370c83b255c3a09f8374ee0c1dfec7ae6d378a
Boris Vedernikov <bove2@alumni.us.es>
2026-03-19 00:26:48 +0100

    fixes for nv anim

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
+  output: 'standalone',
   images: {
     unoptimized: true,
   },
